eade222f7c2d58ffe78c343b80095f798f5088e7
astepniewski <a.stepniewski@hotmail.com>
2015-11-17 18:28:29 +0100

    Add dbContext

---
+++ RestApi/RestApi/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
