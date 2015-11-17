f4353268b510aba8c0776a98d26cb33ebdcbf7f9
astepniewski <a.stepniewski@hotmail.com>
2015-11-17 18:24:36 +0100

    Add Person model

---
+++ RestApi/RestApi/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+    }
+}
