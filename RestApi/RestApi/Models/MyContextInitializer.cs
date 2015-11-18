6d240bcdd943f12c199b45edb9c4c51432d098d0
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-11-18 18:31:22 +0100

    Add database initializer

---
+++ RestApi/RestApi/Models/MyContextInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class MyContextInitializer : DropCreateDatabaseAlways<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext context)
+        {
+            base.Seed(context);
+
+            context.People.Add(new Person
+            {
+                FirstName = "Arkadiusz",
+                LastName = "Stępniewski",
+                Age = 22,
+                Profession = "student"
+            });
+
+            context.People.Add(new Person
+            {
+                FirstName = "Michał",
+                LastName = "Kruk",
+                Age = 32,
+                Profession = "mechanik"
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
